6ea8e8e556cccea278512149da1d13652399c90e
Utsav bhattarai <utsavdotdev@gmail.com>
2026-01-17 22:47:40 +0545

    feat: add responsive styling

--- js/script.js
+++ js/script.js
@@ -10,41 +10,29 @@ class TypeWriter {
   }
 
   type() {
-    // Current index of word
     const current = this.wordIndex % this.words.length;
-    // Get full text of current word
     const fullTxt = this.words[current];
 
-    // Check if deleting
     if (this.isDeleting) {
-      // Remove char
       this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
-      // Add char
       this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    // Insert txt into element
     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-    // Initial Type Speed
     let typeSpeed = 200;
 
     if (this.isDeleting) {
       typeSpeed /= 2;
     }
 
-    // If word is complete
     if (!this.isDeleting && this.txt === fullTxt) {
-      // Make pause at end
       typeSpeed = this.wait;
-      // Set delete to true
       this.isDeleting = true;
     } else if (this.isDeleting && this.txt === "") {
       this.isDeleting = false;
-      // Move to next word
       this.wordIndex++;
-      // Pause before start typing
       typeSpeed = 500;
     }
 
@@ -62,7 +50,6 @@ function init() {
     console.log(words);
     const wait = txtElement.getAttribute("data-wait");
     console.log(wait);
-    // Init TypeWriter
     new TypeWriter(txtElement, words, wait);
   }
 }
